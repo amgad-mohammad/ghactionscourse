95e3c2862fc747d1673ae5dccad0d891ab8dfac0
lenovo <amgad.mohammed2019@gmail.com>
2026-08-09 02:50:07 +0300

    adjust test script

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
     function greet(person) {
-        return `Hello ${person}`
+        return `Hello, ${person}`
     }
 
     module.exports = {greet}
 
     if(require.main === module) {
-        console.log(greet('Mohammed'))
+        console.log(greet('Test!'))
     }
